bfc2032a2ef6f9fc86fe7d3f913f89da01561179
John Wolfe <jwolfe890@gmail.com>
2020-01-06 18:22:37 -0600

    first commit

---
+++ src/pages/homePageComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div style="backgroundColor: purple">
+        <h1>My home page</h1>
+        <p>Some content</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
